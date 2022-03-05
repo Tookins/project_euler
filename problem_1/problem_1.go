8611d18bc6b9f2b72dac2d4b02fda84304c47d77
rjojr <use.the.forc3.luke@gmail.com>
2022-03-05 16:37:46 -0500

    changed the structure

--- problem_1/problem_1.go
+++ problem_1/problem_1.go
@@ -1,14 +1,39 @@
 package problem_1
 
-// return the sum of all the multiples of 3 or 5 
-func Problem01 (ceil int) int {
+import (
+	"os"
+	"strconv"
+)
+
+const fstring = "/home/usetheforc3luke/go/src/project_euler/problem_1/data.txt"
+
+// return the sum of all the multiples of 3 or 5
+func Execute() int {
+
+	in, err := os.ReadFile(fstring)
+
+	check(err)
+
+	data, err := strconv.Atoi(string(in[:len(in)-1]))
+
+	check(err)
+
 	sum := 0
+
 	i := 0
-	for i < ceil {
-		if (i % 3 == 0 || i % 5 == 0) {
+
+	for i < data {
+		if i%3 == 0 || i%5 == 0 {
 			sum += i
 		}
 		i++
 	}
 	return sum
+}
+
+//error checker for files
+func check(e error) {
+	if e != nil {
+		panic(e)
+	}
 }
--- project_euler.go
+++ project_euler.go
@@ -1,18 +1,11 @@
 package main
 
-import(
+import (
 	"fmt"
 	"os"
-	"strconv"
-//	"project_euler/problem_1"
-)
 
-//error checker for files
-func check(e error) {
-	if e != nil {
-		panic(e)
-	}
-}
+	"project_euler/problem_1"
+)
 
 // generalize the process of running the package containing the algorithm
 // for a given problem and providing the correct data to the algorithm
@@ -21,23 +14,12 @@ func main() {
 
 	// parse clarg with format: euler [problem number]
 	problem := os.Args[1]
-	
+
 	switch problem {
-	
-		case "1": {
-			fstring := "/home/usetheforc3luke/go/src/project_euler/problem_1/data.txt"
-			in, err := os.ReadFile(fstring)
 
-			check(err)
-
-			result, err := strconv.Atoi(string(in[:len(in) - 1]))
-
-			check(err)
-
-			fmt.Println(result)
-
-			//result := problem_1.Problem01(n)
-		}
-		default: fmt.Println("Problem number not found.")
+	case "1":
+		fmt.Println(problem_1.Execute())
+	default:
+		fmt.Println("Problem number not found.")
 	}
 }
