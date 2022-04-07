3863c27aaab73604271ceda3ad9a7898cc3453d9
rjojr <use.the.forc3.luke@gmail.com>
2022-04-07 11:00:42 -0400

    problem 12 algorithm runtime fixed, solution still incorrect

--- problem_12/problem_12.go
+++ problem_12/problem_12.go
@@ -8,21 +8,28 @@ import (
 )
 
 func Execute() int64 {
-	//premise: if a number has 
+	//premise: if a number has
 	max_factors := 500
 	var factor_count int
+	max_factor_count := 0
 	min_triangle_num := int64(1)
 
-	for i:=int64(1); factor_count <= max_factors; i++ {
+	for i := int64(1); factor_count <= max_factors; i += i + 1 {
 		factor_count = 0
-		for j:=int64(1); float64(j) <= math.Sqrt(float64(i)); j++ {
+		sqrti := int64(math.Sqrt(float64(i)))
+		for j := int64(1); j <= sqrti; j++ {
 			if i%j == 0 {
 				factor_count += 2
+				if j == sqrti {
+					factor_count -= 1
+				}
 			}
-			//debugging
-			if factor_count > 100 {
-				fmt.Println(i)
-			}
+
+		}
+		if factor_count > max_factor_count {
+			max_factor_count = factor_count
+			fmt.Println(i)
+			fmt.Println(factor_count)
 		}
 		if factor_count > max_factors {
 			min_triangle_num = i
